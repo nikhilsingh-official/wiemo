aea5818d73071bd52ded5cb5cc3f9e2063ebfb55
Nikhil Singh <nikhil.s.abrol@gmail.com>
2026-08-09 17:18:59 +0530

    chore: isolate Nuxt typecheck output

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  buildDir: process.env.NUXT_BUILD_DIR ?? '.nuxt',
   devtools: { enabled: true },
   css: ['~/assets/styles/base.scss'],
 
